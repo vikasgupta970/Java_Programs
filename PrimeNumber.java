91d1c990631b352f776a209a4633803c4b214772
Vikas Gupta <74144103+vikasgupta970@users.noreply.github.com>
2022-02-10 18:46:40 +0530

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,5 +1,7 @@
+                                                    // jAVA PROGRAM TO FIND PRIME NUMBERS BETWEEN GIVEN RANGE
+
 public class PrimeNumber{
-    static void Check(int a){
+    static void Check(int a){                                 //Function to check whether passed number is prime or not 
         boolean flag=false;
       for(int i=2;i<=Math.sqrt(a);i++){
           if(a%i==0){
@@ -11,10 +13,10 @@ public class PrimeNumber{
       } 
     }
     public static void main(String[] args){
-        int low=Integer.parseInt(args[0]);
-        int high=Integer.parseInt(args[1]);
+        int low=Integer.parseInt(args[0]);              // low is the number from which u want Prime numbers
+        int high=Integer.parseInt(args[1]);             // high is the number upto which you want Prime numbers
        for(int i=low;i<=high;i++){
-           Check(i);
+           Check(i);    
        }
     }
 }
